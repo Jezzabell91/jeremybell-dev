c2ddfeac842429c899f05c5856ce84ed1b4dfdf3
Jeremy Bell <jeremy.bell91@gmail.com>
2021-01-04 17:15:26 +1000

    Added content to about page

--- pages/about.js
+++ pages/about.js
@@ -8,62 +8,59 @@ const About = () => {
     <ul className="grid place-items-start grid-cols-1 gap-8 md:grid-cols-2 mb-6">
         <li className="w-4/5">
             <p className="text-lg font-medium leading-6 text-gray-900">
-                What is a home energy rating?
+                What are you most excited for in 2021?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
-                    A home energy rating is an estimated calculation into a homes potential energy usage, which will determine the amount of heating and cooling required to make its occupants comfortable. It produces a star rating dependant on the amount of heating and cooling loads which will be required, from 0 to 10 stars.
+                This year I’m hoping I can get my first job as a junior developer. Signing the contract that for the first job of my new career is going to be an amazing feeling. 
+                Another thing I'm excited about is that I recently got my first VR headset, the HP Reverb G2. I am buzzing that I can fly a spaceship even if it's not quite the real thing.                </p>
+            </p>
+        </li>
+        <li className="w-4/5">
+            <p className="text-lg font-medium leading-6 text-gray-900">
+                What would you want to achieve if you had all of the knowledge, resources and self-belief you needed?
+            </p>
+            <p className="mt-2">
+                <p className="text-base leading-6 text-gray-500">
+                I would help create technologies that bridge the link between our brains and computers and solve problems ranging from medical all the way through to entertainment. I believe Brain Computer Interfaces will become a huge industry in the future and the possibilities are extremely exciting.                 </p>
+            </p>
+        </li>
+        <li className="w-4/5">
+            <p className="text-lg font-medium leading-6 text-gray-900">
+                What are you currently working on?
+            </p>
+            <p className="mt-2">
+                <p className="text-base leading-6 text-gray-500">
+                I am in the final term of Coder Academy’s Fast Track Coding Bootcamp. At the moment we are learning React and are about to start our final projects using MERN stack. Some of things we previously covered include Ruby (which I love), Ruby on Rails, HTML, CSS, JavaScript. It’s been an intense program so I haven’t been able to work on many projects outside of the course but I have plenty of ideas that I want to put my new skills towards.</p>
+            </p>
+        </li>
+        <li className="w-4/5">
+            <p className="text-lg font-medium leading-6 text-gray-900">
+                What do you like to do outside of developing?
+            </p>
+            <p className="mt-2">
+                <p className="text-base leading-6 text-gray-500">
+                I do a lot of reading, and I generally alternate between sci-fi/fantasy, fictionalised history and classics. I’m also a big gamer. At the moment I’m playing Hades on Switch, Doom Eternal on PC and Half Life: Alyx in VR. 
+For outside activities, I enjoy running, lifting weights, bouldering and once a week I play beach volleyball. I’m almost always up for hanging out and grabbing a beer with friends. 
                 </p>
             </p>
         </li>
         <li className="w-4/5">
             <p className="text-lg font-medium leading-6 text-gray-900">
-                Why do I need a 6 Star energy rating?
+                What is a side project you want to work on?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
-                    In most Australian states the government requires that all new homes and apartments (along with certain types of building extensions) built since 2010 be energy rated and achieve a minimum of 6 Stars.
-                </p>
+                I want to develop a running app that uses heart rate training methodologies to increase my running ability and to track my progress through a running program. I recently learned about heart rate training and I have found that running with HR in mind instead of focusing on pace has helped me, but none of the apps I have found do exactly what I want, so why not make my own?                  </p>
             </p>
         </li>
         <li className="w-4/5">
             <p className="text-lg font-medium leading-6 text-gray-900">
-                What is the general cost of an energy rating?
+                What is the thing you could talk endlessy about?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
-                    Simple energy rating prices vary greatly on the size and type of building, generally an energy rating will cost somewhere between $130 to $700+.
-                </p>
-            </p>
-        </li>
-        <li className="w-4/5">
-            <p className="text-lg font-medium leading-6 text-gray-900">
-                What information do I need to supply for an energy rating to be completed??
-            </p>
-            <p className="mt-2">
-                <p className="text-base leading-6 text-gray-500">
-                    The information required to complete a full and comprehensive energy report are the following final working drawings: Site Plan, Floor Plan, Elevations, Sections, Lighting layout and window schedule (including sizes of the existing windows).
-                </p>
-            </p>
-        </li>
-        <li className="w-4/5">
-            <p className="text-lg font-medium leading-6 text-gray-900">
-                Does building an extension need an energy rating?
-            </p>
-            <p className="mt-2">
-                <p className="text-base leading-6 text-gray-500">
-                    Depended on the size of the extension you are building there is a chance that it too will need to be energy rated. It&#x27;s always best to check first before going ahead with construction.
-                </p>
-            </p>
-        </li>
-        <li className="w-4/5">
-            <p className="text-lg font-medium leading-6 text-gray-900">
-                What is the general cost of an energy rating?
-            </p>
-            <p className="mt-2">
-                <p className="text-base leading-6 text-gray-500">
-                    Depended on the size of the extension you are building there is a chance that it too will need to be energy rated. It&#x27;s always best to check first before going ahead with construction.
-                </p>
+                I have an obsession/passion for the reality TV show Survivor. I can list every season and winner in order, my personal rankings for best players and can discuss in depth some of the best and worst moves that players have made over the shows 40 seasons.  I really love games that require high levels of social and strategic ability and combining it with physical challenges and the deprivation of living on an island with limited food and water makes for amazing television in my opinion.                </p>
             </p>
         </li>
     </ul>
