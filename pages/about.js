2e6021ed6cc93a36c982691b78c593e50200acd4
Jeremy Bell <jeremy.bell91@gmail.com>
2021-02-23 11:26:41 +1000

    Added styling to external links in text

--- pages/about.js
+++ pages/about.js
@@ -18,42 +18,47 @@ const About = () => {
         </li>
         <li className="lg:w-4/5">
             <p className="text-xl font-medium leading-6 text-gray-900">
-                What technologies do you like working with?
+                What are you currently working on?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
-                I recently completed a project using the MERN stack and I enjoyed the flexibility of using Express and MongoDB in the backend to develop the API and pairing it with React in the frontend. 
-                I like that React has an extensive community and I found using packages like <a href="https://react-hook-form.com/">React Hook Form</a> made the development experience better. 
-                Other than that, I really like using vanilla JavaScript with HTML and TailwindCSS. My first love in programming was Ruby and I wish it was more widely used because I find it    
+                I just finished Coder Academy’s Fast Track Coding Bootcamp. At the moment I am focusing on some side projects inlcuding, <a className="underline transition duration-200 hover:text-red-400 hover:no-underline" href="https://github.com/Jezzabell91/heart-rate-training">Heart Rate Trainer</a>, a PWA for running that uses the <a className="underline transition duration-200 hover:text-red-400 hover:no-underline" href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API">Web Bluetooth API</a> to connect to a heart rate monitor. 
+                I'm enjoying developing something that I use almost every day and incrementally improving it.  
                 </p>
             </p>
         </li>
+        
         <li className="lg:w-4/5">
             <p className="text-xl font-medium leading-6 text-gray-900">
                 What did you use to create this website?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
-                For this site I felt like trying some technologies that I wasn't familiar with. The website is built with Next.js and hosted on Vercel. I was interested in how Next.js handled <a href="https://nextjs.org/docs/routing/introduction">routing and linking between pages</a>. 
-                I was also interested in using new image formats. All feature images on the site have AVIF and WebP versions created using <a href="https://squoosh.app/">Squoosh</a>. They also have a back up .jpg that are used for browsers that aren't compatible with the newer technologies. 
-                AVIF images are much smaller in size but have the same visual quality, however at the moment they are only <a href="https://caniuse.com/avif">supported</a> by the current versions of Chrome, Firefox and Opera. Styling has been done with TailwindCSS.               
+                 For this site I felt like trying some technologies that I wasn't familiar with as well. The website is built with Next.js and hosted on Vercel and styling has been done with TailwindCSS. I was interested in how Next.js handled <a className="underline transition duration-200 hover:text-red-400 hover:no-underline" href="https://nextjs.org/docs/routing/introduction">routing and linking between pages</a>. 
+                </p>
+                <p className="text-base leading-6 text-gray-500">
+                I was also interested in using new image formats. All feature images on this site have AVIF and WebP versions created using <a className="underline transition duration-200 hover:text-red-400 hover:no-underline" href="https://squoosh.app/">Squoosh</a>. They also have a back up .jpg that are used for browsers that aren't compatible with the newer technologies. 
+                AVIF images are much smaller in size but have the same visual quality, however at the moment they are only <a className="underline transition duration-200 hover:text-red-400 hover:no-underline" href="https://caniuse.com/avif">supported</a> by the current versions of Chrome, Firefox and Opera.               
                 </p>
             </p>
         </li>
         <li className="lg:w-4/5">
             <p className="text-xl font-medium leading-6 text-gray-900">
-                What are you currently working on?
+                What webdev technologies do you like?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
-                I just finished Coder Academy’s Fast Track Coding Bootcamp. At the moment I am focusing on some side projects inlcuding, <a href="https://github.com/Jezzabell91/heart-rate-training">Heart Rate Trainer</a>, a PWA for running that uses the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API">Web Bluetooth API</a> to connect to a heart rate monitor. 
-                I'm enjoying developing something that I use almost every day and incrementally improving it.  
+                I recently completed a project using the MERN stack and I enjoyed the flexibility of using Express and MongoDB in the backend to develop the API and pairing it with React in the frontend. 
+                I like that React has an extensive community and I found using packages like <a className="underline transition duration-200 hover:text-red-400 hover:no-underline" href="https://react-hook-form.com/">React Hook Form</a> made the development experience better.
+                </p> 
+                <p className="text-base leading-6 text-gray-500">
+                I really like using vanilla JavaScript with HTML and TailwindCSS however my first love in programming was Ruby.    
                 </p>
             </p>
         </li>
         <li className="lg:w-4/5">
             <p className="text-xl font-medium leading-6 text-gray-900">
-                What do you like to do outside of developing?
+                What do you do outside of developing?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
@@ -64,7 +69,7 @@ For outside activities, I enjoy running, lifting weights, bouldering and once a 
         </li>
         <li className="lg:w-4/5">
             <p className="text-xl font-medium leading-6 text-gray-900">
-                What is the thing you could talk endlessy about?
+                What's something you can talk endlessy about?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
