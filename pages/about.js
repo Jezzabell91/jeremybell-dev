65c6baa641474aaf3fbba17583457dcaaeeddba3
Jeremy Bell <jeremy.bell91@gmail.com>
2021-02-23 11:08:07 +1000

    Updated about page

--- pages/about.js
+++ pages/about.js
@@ -18,11 +18,26 @@ const About = () => {
         </li>
         <li className="lg:w-4/5">
             <p className="text-xl font-medium leading-6 text-gray-900">
-                What would you want to achieve if you had all of the knowledge, resources and self-belief you needed?
+                What technologies do you like working with?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
-                I would help create technologies that bridge the link between our brains and computers and solve problems ranging from medical all the way through to entertainment. I believe Brain Computer Interfaces will become a huge industry in the future and the possibilities are extremely exciting.                 </p>
+                I recently completed a project using the MERN stack and I enjoyed the flexibility of using Express and MongoDB in the backend to develop the API and pairing it with React in the frontend. 
+                I like that React has an extensive community and I found using packages like <a href="https://react-hook-form.com/">React Hook Form</a> made the development experience better. 
+                Other than that, I really like using vanilla JavaScript with HTML and TailwindCSS. My first love in programming was Ruby and I wish it was more widely used because I find it    
+                </p>
+            </p>
+        </li>
+        <li className="lg:w-4/5">
+            <p className="text-xl font-medium leading-6 text-gray-900">
+                What did you use to create this website?
+            </p>
+            <p className="mt-2">
+                <p className="text-base leading-6 text-gray-500">
+                For this site I felt like trying some technologies that I wasn't familiar with. The website is built with Next.js and hosted on Vercel. I was interested in how Next.js handled <a href="https://nextjs.org/docs/routing/introduction">routing and linking between pages</a>. 
+                I was also interested in using new image formats. All feature images on the site have AVIF and WebP versions created using <a href="https://squoosh.app/">Squoosh</a>. They also have a back up .jpg that are used for browsers that aren't compatible with the newer technologies. 
+                AVIF images are much smaller in size but have the same visual quality, however at the moment they are only <a href="https://caniuse.com/avif">supported</a> by the current versions of Chrome, Firefox and Opera. Styling has been done with TailwindCSS.               
+                </p>
             </p>
         </li>
         <li className="lg:w-4/5">
@@ -31,7 +46,9 @@ const About = () => {
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
-                I am in the final term of Coder Academy’s Fast Track Coding Bootcamp. At the moment we are learning React and are about to start our final projects using MERN stack. Some of things we previously covered include Ruby (which I love), Ruby on Rails, HTML, CSS, JavaScript. It’s been an intense program so I haven’t been able to work on many projects outside of the course but I have plenty of ideas that I want to put my new skills towards.</p>
+                I just finished Coder Academy’s Fast Track Coding Bootcamp. At the moment I am focusing on some side projects inlcuding, <a href="https://github.com/Jezzabell91/heart-rate-training">Heart Rate Trainer</a>, a PWA for running that uses the <a href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API">Web Bluetooth API</a> to connect to a heart rate monitor. 
+                I'm enjoying developing something that I use almost every day and incrementally improving it.  
+                </p>
             </p>
         </li>
         <li className="lg:w-4/5">
@@ -47,20 +64,11 @@ For outside activities, I enjoy running, lifting weights, bouldering and once a 
         </li>
         <li className="lg:w-4/5">
             <p className="text-xl font-medium leading-6 text-gray-900">
-                What is a side project you want to work on?
-            </p>
-            <p className="mt-2">
-                <p className="text-base leading-6 text-gray-500">
-                I want to develop a running app that uses heart rate training methodologies to increase my running ability and to track my progress through a running program. I recently learned about heart rate training and I have found that running with HR in mind instead of focusing on pace has helped me, but none of the apps I have found do exactly what I want, so why not make my own?                  </p>
-            </p>
-        </li>
-        <li className="lg:w-4/5">
-            <p className="text-xl font-medium leading-6 text-gray-900">
                 What is the thing you could talk endlessy about?
             </p>
             <p className="mt-2">
                 <p className="text-base leading-6 text-gray-500">
-                I have an obsession/passion for the reality TV show Survivor. I can list every season and winner in order, my personal rankings for best players and can discuss in depth some of the best and worst moves that players have made over the shows 40 seasons.  I really love games that require high levels of social and strategic ability and combining it with physical challenges and the deprivation of living on an island with limited food and water makes for amazing television in my opinion.                </p>
+                A bit weird, but I have an obsession/passion for the reality TV show Survivor. I honestly could go on for days talking about it, so if you ever feel like talking to someone about Survivor please hit me up.</p>
             </p>
         </li>
     </ul>
