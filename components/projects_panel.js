76bcd022698370f058d9ebf034f6cba4f1d9d9f2
Jeremy <jeremy.bell91@gmail.com>
2022-08-25 15:51:01 +1000

    Add FFMPEG Trim and Compress to projects

--- components/projects_panel.js
+++ components/projects_panel.js
@@ -4,7 +4,7 @@ import ProjectDetails from './project_details'
 
 const ProjectsPanel = () => {
    const projects = Projects
-   const [hr_trainer, cowgo, steggyhide, cityclimb] = projects
+   const [ffmpeg_trim_and_compress, hr_trainer, cowgo, steggyhide, cityclimb] = projects
 
 const names = projects.map(item => item.name)
 
@@ -22,6 +22,7 @@ const [activeTab, setActiveTab] = useState(0)
       <option>{`${names[1]}`}</option>
       <option>{`${names[2]}`}</option>
       <option>{`${names[3]}`}</option>
+      <option>{`${names[4]}`}</option>
     </select>
   </div>
   <div className="hidden sm:block">
@@ -45,15 +46,20 @@ const [activeTab, setActiveTab] = useState(0)
         <span aria-hidden="true" className={`${activeTab === 3 ? 'bg-indigo-500 absolute inset-x-0 bottom-0 h-0.5' : 'bg-transparent absolute inset-x-0 bottom-0 h-0.5'}`}></span>
       </button>
 
+      <button onClick={() => setActiveTab(3)} aria-current={`${activeTab === 4 ? 'true' : 'false'}`} className={`${activeTab === 3 ? 'tabActive' : 'tabNotActive'} rounded-r-lg`}>
+        <span>{`${names[4]}`}</span>
+        <span aria-hidden="true" className={`${activeTab === 4 ? 'bg-indigo-500 absolute inset-x-0 bottom-0 h-0.5' : 'bg-transparent absolute inset-x-0 bottom-0 h-0.5'}`}></span>
+      </button>
     </nav>
   </div>
 </div>
   </div>
   <div className="px-4 py-5 sm:p-6">
     {/* <!-- Content goes here --> */}
-    {activeTab === 0 ? <ProjectDetails project={hr_trainer} /> : 
-        activeTab === 1 ? <ProjectDetails project={cowgo} /> :
-        activeTab === 2 ? <ProjectDetails project={steggyhide} /> : <ProjectDetails project={cityclimb} />}
+    {activeTab === 0 ? <ProjectDetails project={ffmpeg_trim_and_compress} /> : 
+        activeTab === 1 ? <ProjectDetails project={hr_trainer} /> :
+        activeTab === 2 ? <ProjectDetails project={cowgo} /> :
+        activeTab === 3 ?  <ProjectDetails project={steggyhide} /> : <ProjectDetails project={cityclimb} />}
   </div>
 </div>
 )
