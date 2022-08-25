d64cdb4bfb2c02b34167487bfc8a02e31f015ead
Jeremy <jeremy.bell91@gmail.com>
2022-08-25 15:52:53 +1000

    Add second image to blog posts

--- _posts/posts.js
+++ _posts/posts.js
@@ -3,7 +3,8 @@
         title: 'Why I Loved Coder Academy’s Coding Bootcamp',
         slug: 'why-i-loved-coder-academys-coding-bootcamp',
         date: 'Thu Mar 18 2021',
-        image: "coderacademy_bris",
+        image1: "coderacademy_bris",
+        image2: "coderacademy_bris",
         content: ["In February 2021 I finished Coder Academy’s 6 month long Fast Track Coding Bootcamp and it was one of the best educational experiences I have had. It was tough, and it occasionally pushed my limits, but it was rewarding, and I have left with the confidence that I am on the right path for future learning and my career.", "The thing that I liked most was of course the people I shared the experience with. In University I found it rare to find people that were driven and determined to push themselves. This was not the case at Coder Academy. Most people put in a tremendous amount of effort to learn the content and take the most from the opportunity. For me, being surrounded by hard working peers was a source of motivation and the environment was much more collaborative than it was competitive.", "Could I have learnt the content on my own? Yes and no. There was nothing content wise that was at a level that I could not have understood through self-learning, but I also know that in my previous self-learning efforts that every time I reached a significant roadblock I would struggle with motivation and self-belief. Having the educators and other students there to help when things got hard helped me push through those roadblocks. So yeah, I could have probably done it by myself but it would have taken years instead of months and so I think the investment in the course was worth it in terms of economic value and my own sanity."]
     },
     {
